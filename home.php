48d5c97d0e44e8fa334e467c5d9c3a4753e4164a
dinhphuong98 <phuong1998bn@gmail.com>
2019-03-25 22:48:26 +0700

    php

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php include('header.php'); ?>
+<link rel="stylesheet" type="text/css" href="css/style2.css">
+<div class="dashboard-top">
+  <div class="row clearfix">
+                
+</div>
+    <div class="pie-report" style="float: right;">
+        <script src="js/jquery.min.js"></script>
+        <?php include('script.php'); ?> 
+
+       
+
+
+        
+        </body>
+</html>

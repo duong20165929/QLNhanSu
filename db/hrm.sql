7b63a6786ecee4d2d737ec75494f40e5962398db
= <=>
2019-05-01 18:53:39 +0700

    commit them

--- db/hrm.sql
+++ db/hrm.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Feb 28, 2017 at 04:34 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.20
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 

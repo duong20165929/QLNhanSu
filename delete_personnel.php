8e666c18b0a9fa8027944acaac76dd79652c6045
Nam <nampro19898@gmail.com>
2019-05-12 00:24:09 +0700

    change delete person

--- delete_personnel.php
+++ delete_personnel.php
@@ -4,5 +4,5 @@ include('connect.php');
 $id = $_GET['per_id'];
 	$delete = $con->prepare("DELETE FROM tbl_personnel WHERE per_id = '$id'");
 	$delete->execute();
-	header('location:fortune_towne_campus.php');
+	header('location:all_personnel.php');
 ?>

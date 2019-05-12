d52eddc1836746330b920c7a5e9d7e8007280941
michellinh <linhjava98@gmail.com>
2019-05-12 10:22:01 +0700

    edit_rank

--- edit_rank.php
+++ edit_rank.php
@@ -1,6 +1,17 @@
 <a class="btn btn-success" href="#edit<?php echo $row['rank_id']?>" data-toggle="modal">
     <span class = "glyphicon glyphicon-pencil" aria-hidden = "true">Edit</span>
 </a> 
+<span> <button class="btn btn-danger btn-sm remove" onclick="return deleletconfig()">Delete</button></span>
+<script>
+function deleletconfig(){
+
+var del=confirm("Are you sure you want to delete this record?");
+if (del==true){
+    window.location.href="delete_rank.php?rank_id=<?php echo $row['rank_id']?>";
+}
+return del;
+}
+</script>
 <!-- Modal -->
 <div class="modal fade" id="edit<?php echo $row['rank_id']?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-md">

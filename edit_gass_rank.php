ef043cb15053ff1fec6248f4915ca1d435ed8ff4
michellinh <linhjava98@gmail.com>
2019-05-12 10:22:49 +0700

    edit_gass_rank

--- edit_gass_rank.php
+++ edit_gass_rank.php
@@ -1,6 +1,19 @@
 <a class="btn btn-success" href="#edit<?php echo $row['gass_id']?>" data-toggle="modal">
     <span class = "glyphicon glyphicon-pencil" aria-hidden = "true">Edit</span>
+    
 </a> 
+<span> <button class="btn btn-danger btn-sm remove" onclick="return deleletconfig()">Delete</button></span>
+<script>
+function deleletconfig(){
+
+var del=confirm("Are you sure you want to delete this record?");
+if (del==true){
+    window.location.href="delete_gass_rank_query.php?gass_id=<?php echo $row['gass_id']?>";
+}
+return del;
+}
+</script>
+
 <!-- Modal -->
 <div class="modal fade" id="edit<?php echo $row['gass_id']?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-md">
@@ -9,7 +22,7 @@
             <div class = "panel panel-primary">
                 <div class = "panel-heading">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4>C?p nh?t ch?c danh</h4>
+                    <h4>UPDATE GASS RANK</h4>
                 </div>
             </div>
             <div class="modal-body">          
@@ -34,5 +47,4 @@
         </div>
     </div>
 </div>
-
 

6d4be7107b34b89a41b30f503730ba669d4db653
cuongcx98 <cxchtc1998@gmail.com>
2019-05-12 19:12:21 +0700

    change format font to utf8

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 	$host = "localhost";
 	$user = "root";
 	$pass = "";
-
+	$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 	try {
 		$con = new PDO("mysql:host=$host;dbname=hrm", $user, $pass);
 		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
